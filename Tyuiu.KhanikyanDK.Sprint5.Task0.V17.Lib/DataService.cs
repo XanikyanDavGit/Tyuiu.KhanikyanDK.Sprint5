653632c06e65010697c99bc7169cbb1aedb5812b
xanik <xanikyan00@gmail.com>
2025-11-02 16:52:04 +0500

    Task0

--- Tyuiu.KhanikyanDK.Sprint5.Task0.V17.Lib/DataService.cs
+++ Tyuiu.KhanikyanDK.Sprint5.Task0.V17.Lib/DataService.cs
@@ -8,17 +8,17 @@ namespace Tyuiu.KhanikyanDK.Sprint5.Task0.V17.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            // вычисляем значение функции F(x) = 2.4x^3 + 0.4x^2 - 1.4x + 4.1
-            double result = 2.4 * Math.Pow(x, 3) + 0.4 * Math.Pow(x, 2) - 1.4 * x + 4.1;
+            double result = 2.4 * Math.Pow(x, 3)
+                           + 0.4 * Math.Pow(x, 2)
+                           - 1.4 * x
+                           + 4.1;
             result = Math.Round(result, 3);
 
-            // создаём путь к файлу
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            // создаём путь во временной папке
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
 
-            // записываем результат в файл
             File.WriteAllText(path, result.ToString());
 
-            // возвращаем путь для отображения в консоли
             return path;
         }
     }
